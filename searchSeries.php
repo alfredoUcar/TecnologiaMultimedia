a87d0b1382c3035297550f18e3febc9e65310a6d
alfredo <alfredo.ucar@gmail.com>
2014-04-19 14:01:23 +0200

    activacion api series.ly y formulario simple de busqueda(sin procesameinto XML)

---
+++ searchSeries.php
@@ -0,0 +1,10 @@
+<div>
+    <form name="search-series">
+        <input type="text" name="query" placeholder="Escriba el texto a buscar" >
+        <input type="submit" value="Buscar">
+    </form>
+</div>
+
+<div id="resultados">
+    <p>Aquí aparecerán los resultados de la búsqueda</p>
+</div>
